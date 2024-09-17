83936f25f9f475487844244429d283f6e024f31b
DonPombo <alejandropombo31@gmail.com>
2024-09-17 09:24:51 -0400

    se agrego Firebase y Firestore al proyecto funcionando todo a la perfeccion

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:un_lugar_chido_app/presentation/screens.dart';
+//importaciones de Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const RestauranteMexicanoApp());
 }
 
